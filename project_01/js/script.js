2a497074425f1d9f646bc40b3703ae923ce6b2d6
illia_Klepach <75541887+klepa4@users.noreply.github.com>
2020-12-19 16:54:12 +0100

    Update script.js

--- project_01/js/script.js
+++ project_01/js/script.js
@@ -9,13 +9,13 @@ var p = document.querySelector('.text-quote');
 var button = document.querySelector('button');
 
 var quotes = [
-  "Be yourself; everyone else is already taken.",
-  "So many books, so little time.",
-  "Be the change that you wish to see in the world.",
-  "A room without books is like a body without a soul.",
-  "In three words I can sum up everything I've learned about life: it goes on.",
-  "If you tell the truth, you don't have to remember anything.",
-  "A friend is someone who knows all about you and still loves you.",
+  'Be yourself; everyone else is already taken.',
+  'So many books, so little time.',
+  'Be the change that you wish to see in the world.',
+  'A room without books is like a body without a soul.',
+  'In three words I can sum up everything I've learned about life: it goes on.',
+  'If you tell the truth, you don't have to remember anything.',
+  'A friend is someone who knows all about you and still loves you.',
 ];
 
 function getRandomQuote(){
