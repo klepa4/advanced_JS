3cdd20621ea3bdae4c776b73bd0d8f984201346f
illia_Klepach <75541887+klepa4@users.noreply.github.com>
2020-12-19 16:43:51 +0100

    Add files via upload

---
+++ project_01/js/script.js
@@ -0,0 +1,34 @@
+ // 1. navigation - done
+// 2. add event listener - done
+// 3. create data structure with quotes - done
+// 4. get random quote
+// 5. display random quote
+
+// Navigation
+var p = document.querySelector('.text-quote');
+var button = document.querySelector('button');
+
+var quotes = [
+  "Be yourself; everyone else is already taken.",
+  "So many books, so little time.",
+  "Be the change that you wish to see in the world.",
+  "A room without books is like a body without a soul.",
+  "In three words I can sum up everything I've learned about life: it goes on.",
+  "If you tell the truth, you don't have to remember anything.",
+  "A friend is someone who knows all about you and still loves you.",
+];
+
+function getRandomQuote(){
+    // logic here
+ 	var getNumber = Math.floor(Math.random() * quotes.length);
+    p.innerHTML = quotes[getNumber];
+}
+
+
+// Get random quote on init
+getRandomQuote();
+console.log(getRandomQuote());
+
+// Add event listener for random quote button
+button.addEventListener('click', getRandomQuote);
+
