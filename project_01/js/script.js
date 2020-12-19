0c92851f80f6a81c7fb644d2e79158b8889f1513
illia_Klepach <75541887+klepa4@users.noreply.github.com>
2020-12-19 17:01:56 +0100

    Update script.js

--- project_01/js/script.js
+++ project_01/js/script.js
@@ -13,8 +13,8 @@ var quotes = [
   'So many books, so little time.',
   'Be the change that you wish to see in the world.',
   'A room without books is like a body without a soul.',
-  'In three words I can sum up everything I've learned about life: it goes on.',
-  'If you tell the truth, you don't have to remember anything.',
+  'In three words I can sum up everything I-ve learned about life: it goes on.',
+  'If you tell the truth, you don-t have to remember anything.',
   'A friend is someone who knows all about you and still loves you.',
 ];
 
